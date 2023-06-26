2ea55d9e0316ac69440ec9592fa996ec37d6ba70
saurav munde <122808971+sauravmunde2023@users.noreply.github.com>
2023-06-26 18:17:13 +0530

    pracice1.java

---
+++ practice1.java
@@ -0,0 +1,26 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class practice1 {
+     public static void main(String[] args) {
+        int a=5;
+        int b=30;
+        int v=525;
+        if(a<b){
+          System.out.println("*");
+          System.out.println("this is s bad thots to destroy in your life");
+        }
+        else{
+
+          System.out.println("hello");
+          System.out.println("hello my friends ");
+          System.out.println(a+b>b);
+     
+        }
+
+              int d=5;
+              int m=10;
+              System.out.println(d+m); 
+              System.out.println(d*m);
+
+          }  
+            }
